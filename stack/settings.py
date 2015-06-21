e7a90dc47c2738fc918c2035c49d93209381d0c1
ilopezluna <ignasi.lopez.luna@gmail.com>
2015-06-21 17:24:25 +0200

    Using mongoDB to store scraped questions

--- stack/settings.py
+++ stack/settings.py
@@ -15,3 +15,10 @@ NEWSPIDER_MODULE = 'stack.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'stack (+http://www.yourdomain.com)'
+
+ITEM_PIPELINES = ['stack.pipelines.MongoDBPipeline', ]
+
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "stackoverflow"
+MONGODB_COLLECTION = "questions"
